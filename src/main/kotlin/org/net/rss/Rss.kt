2e00eacc7f114143646a14133e0452d9607492ae
Smelly Cat <smelly@c.at>
2020-09-23 17:02:06 +1000

    Use xpath

--- src/main/kotlin/org/net/rss/Rss.kt
+++ src/main/kotlin/org/net/rss/Rss.kt
@@ -1,28 +1,31 @@
 package org.net.rss
 
 import org.w3c.dom.Document
-import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
 
 class Rss(rss: String) {
-    val doc: Document
+    private val doc: Document
     val category: String
 
     init {
         doc = getXmlDoc(rss)
-        val tag = doc.getElementsByTagName("category")
-        var c = "missing feed category"
-        for (i in 0..tag.length - 1) {
-            if (tag.item(i).nodeType == Node.ELEMENT_NODE) {
-                c = tag.item(i).textContent
-            }
-        }
-        category = c
+        category = get("/rss/channel/category")
     }
 
-    fun getXmlDoc(rss: String): Document {
+    private fun get(path: String): String {
+        val xpFactory = XPathFactory.newInstance()
+        val xPath = xpFactory.newXPath()
+
+        val nodes = xPath.evaluate(path, doc, XPathConstants.NODESET) as NodeList
+        return nodes.item(0).textContent
+    }
+
+    private fun getXmlDoc(rss: String): Document {
         val dbFactory = DocumentBuilderFactory.newInstance()
         val dBuilder = dbFactory.newDocumentBuilder()
         val xmlInput = InputSource(StringReader(rss))
@@ -32,5 +35,4 @@ class Rss(rss: String) {
 
         return doc
     }
-
 }
