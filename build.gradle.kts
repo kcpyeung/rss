3dec99937ace627bf0836ef8c5ba61d8664e8427
Smelly Cat <smelly@c.at>
2020-09-24 12:07:51 +1000

    Specify kotlin compiler options. Replace obsolete gradle command.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,9 @@ repositories {
 }
 
 dependencies {
-    testCompile("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    //testRuntime("org.junit.platform:junit-platform-runner:1.7.0")
 }
 
 tasks.test {
@@ -19,3 +18,10 @@ tasks.test {
     }
 }
 
+tasks.compileKotlin {
+    kotlinOptions {
+        apiVersion = "1.4"
+        languageVersion = "1.4"
+        jvmTarget = "11"
+    }
+}
