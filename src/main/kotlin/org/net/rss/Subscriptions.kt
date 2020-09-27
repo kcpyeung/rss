66a6fb53828c781a8b4468754fa890bd5eeb36d9
Smelly Cat <smelly@c.at>
2020-09-27 15:16:32 +1000

    Add Subscriptions

---
+++ src/main/kotlin/org/net/rss/Subscriptions.kt
@@ -0,0 +1,8 @@
+package org.net.rss
+
+class Subscriptions {
+    val all = arrayOf(
+      "https://www.theguardian.com/au/rss",
+      "https://www.theage.com.au/rss/national/victoria.xml",
+    )
+}
