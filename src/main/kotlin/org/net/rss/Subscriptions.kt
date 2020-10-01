b19a9fa86a71290ca543e919f7712b58a8975037
Smelly Cat <smelly@c.at>
2020-10-01 17:49:20 +1000

    Add more feeds

--- src/main/kotlin/org/net/rss/Subscriptions.kt
+++ src/main/kotlin/org/net/rss/Subscriptions.kt
@@ -6,5 +6,8 @@ object Subscriptions {
     val all = listOf(
       Subscription("https://www.theguardian.com/au/rss", DateTimeFormatter.RFC_1123_DATE_TIME),
       Subscription("https://www.theage.com.au/rss/national/victoria.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
+      Subscription("https://www.theage.com.au/rss/national.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
+      Subscription("https://www.theage.com.au/rss/culture.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
+      Subscription("https://www.theage.com.au/rss/lifestyle.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
     )
 }
