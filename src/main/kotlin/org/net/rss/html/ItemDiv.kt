596e6709753ce399b35a73a2f322844400b8342c
Smelly Cat <smelly@c.at>
2020-10-02 04:45:06 +1000

    Simply item div creation

--- src/main/kotlin/org/net/rss/html/ItemDiv.kt
+++ src/main/kotlin/org/net/rss/html/ItemDiv.kt
@@ -6,15 +6,14 @@ class ItemDiv(index: Int, private val item: Item, feedDiv: FeedDiv) {
     private val id = feedDiv.id + "-" + index
 
     fun asHtml(): String {
-        val buffer = StringBuffer(1024)
-
-        buffer.append("<div id=\"${id}\">").append("\n")
-        buffer.append("    <div><a href=\"${item.link}\">${item.title}</a></div>").append("\n")
-        buffer.append("    <div>${item.description}</div>").append("\n")
-        buffer.append("    <div>Published at ${item.pubDate}</div>").append("\n")
-        buffer.append("    <div><p/></div>").append("\n")
-        buffer.append("</div>").append("\n")
-
-        return buffer.toString()
+        return """
+            <div id="${id}">
+                <div><a href="${item.link}">${item.title}</a></div>
+                <div>${item.description}</div>
+                <div>Published at ${item.pubDate}</div>
+                <div><p/></div>
+            </div>
+            
+        """.trimIndent()
     }
 }
