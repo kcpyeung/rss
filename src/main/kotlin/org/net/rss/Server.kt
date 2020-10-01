2e44b8ae074d7dd1218b40b859753e676ed29c9a
Smelly Cat <smelly@c.at>
2020-10-01 17:39:23 +1000

    Poll in the background

--- src/main/kotlin/org/net/rss/Server.kt
+++ src/main/kotlin/org/net/rss/Server.kt
@@ -8,15 +8,25 @@ import org.http4k.server.asServer
 import org.net.rss.data.InMemoryFeedRepository
 import org.net.rss.html.FeedDiv
 import java.time.LocalTime
+import kotlin.concurrent.thread
 
 fun main() {
     println("${LocalTime.now()}: Retrieving feeds, please wait...")
     Poller().poll()
     println("${LocalTime.now()}: Done.")
 
+    thread {
+        while (true) {
+            Thread.sleep(FIVE_MINUTES)
+            Poller().poll()
+        }
+    }
+
     println("Server starting on port 8000.")
     app.asServer(SunHttp(8000)).start()
 }
+
+const val FIVE_MINUTES = 5 * 60 * 1000L
 
 val app: HttpHandler = { _ ->
     Response(Status.OK).body(Page(feedDivs()).asHtml())
