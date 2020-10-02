4a1f796257fcb2e6efdc1051deaa403b23a36769
Smelly Cat <smelly@c.at>
2020-10-02 17:40:01 +1000

    Remove query parameters from theage.com.au

--- src/main/kotlin/org/net/rss/Subscriptions.kt
+++ src/main/kotlin/org/net/rss/Subscriptions.kt
@@ -3,11 +3,13 @@ package org.net.rss
 import java.time.format.DateTimeFormatter
 
 object Subscriptions {
+    private val removeQueries: (String) -> String = { it.substringBefore("?") }
+
     val all = listOf(
       Subscription("https://www.theguardian.com/au/rss", DateTimeFormatter.RFC_1123_DATE_TIME),
-      Subscription("https://www.theage.com.au/rss/national/victoria.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
-      Subscription("https://www.theage.com.au/rss/national.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
-      Subscription("https://www.theage.com.au/rss/culture.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
-      Subscription("https://www.theage.com.au/rss/lifestyle.xml", DateTimeFormatter.RFC_1123_DATE_TIME),
+      Subscription("https://www.theage.com.au/rss/national/victoria.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
+      Subscription("https://www.theage.com.au/rss/national.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
+      Subscription("https://www.theage.com.au/rss/culture.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
+      Subscription("https://www.theage.com.au/rss/lifestyle.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
     )
 }
