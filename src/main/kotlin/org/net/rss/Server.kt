eac468b703a7df4eba0a60c69151bb133d397ddb
Smelly Cat <smelly@c.at>
2020-10-03 20:56:05 +1000

    Expose 'mark read' route.

--- src/main/kotlin/org/net/rss/Server.kt
+++ src/main/kotlin/org/net/rss/Server.kt
@@ -1,8 +1,12 @@
 package org.net.rss
 
-import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Status.Companion.SEE_OTHER
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import org.net.rss.data.InMemoryFeedRepository
@@ -28,13 +32,21 @@ fun main() {
 
 const val FIVE_MINUTES = 5 * 60 * 1000L
 
-val app: HttpHandler = { _ ->
-    Response(Status.OK).body(Page(feedDivs()).asHtml())
-}
+val app = routes(
+  "/" bind GET to { _ -> Response(OK).body(Page(feedDivs()).asHtml()) },
+  "/read/{feed:.*}/{item:.*}" bind GET to { request -> markRead(request.path("feed"), request.path("item")) },
+)
 
 private fun feedDivs(): List<FeedDiv> {
     return Subscriptions
       .all
-      .mapNotNull { InMemoryFeedRepository.get(it.url) }
+      .mapNotNull { InMemoryFeedRepository.get(it.feedIdGen(it.url)) }
       .map { FeedDiv(it) }
+}
+
+fun markRead(feed: String?, item: String?): Response {
+    if (feed != null && item != null) {
+        InMemoryFeedRepository.deleteTo(feed, item)
+    }
+    return Response(SEE_OTHER).header("Location", "/")
 }
