eba8ab3f9724a0647b5b6694ea2318767c6fe527
Smelly Cat <smelly@c.at>
2020-10-05 17:05:57 +1100

    Remove redundant date formatter value.

--- src/main/kotlin/org/net/rss/Subscriptions.kt
+++ src/main/kotlin/org/net/rss/Subscriptions.kt
@@ -3,7 +3,6 @@ package org.net.rss
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import java.time.format.DateTimeFormatter
 
 class Subscriptions(yaml: String?) {
     constructor() : this(null)
@@ -15,11 +14,11 @@ class Subscriptions(yaml: String?) {
     init {
         if (yaml == null) {
             all = listOf(
-              Subscription("https://www.theguardian.com/au/rss", DateTimeFormatter.RFC_1123_DATE_TIME),
-              Subscription("https://www.theage.com.au/rss/national/victoria.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
-              Subscription("https://www.theage.com.au/rss/national.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
-              Subscription("https://www.theage.com.au/rss/culture.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
-              Subscription("https://www.theage.com.au/rss/lifestyle.xml", DateTimeFormatter.RFC_1123_DATE_TIME, removeQueries),
+              Subscription("https://www.theguardian.com/au/rss"),
+              Subscription("https://www.theage.com.au/rss/national/victoria.xml", linkRewrite = removeQueries),
+              Subscription("https://www.theage.com.au/rss/national.xml", linkRewrite = removeQueries),
+              Subscription("https://www.theage.com.au/rss/culture.xml", linkRewrite = removeQueries),
+              Subscription("https://www.theage.com.au/rss/lifestyle.xml", linkRewrite = removeQueries),
             )
         } else {
             val objectMapper = ObjectMapper(YAMLFactory())
