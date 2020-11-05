c1442bcf029133985ae8bebdf65108d9d517025f
Smelly Cat <smelly@c.at>
2020-11-05 15:43:57 +1100

    Control shutdown of thread pool to fix memory leak.

--- src/main/kotlin/org/net/rss/Poller.kt
+++ src/main/kotlin/org/net/rss/Poller.kt
@@ -6,22 +6,32 @@ import org.net.rss.io.Fetcher
 import java.net.http.HttpClient
 import java.time.LocalTime
 import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class Poller(subscriptions: List<Subscription>) {
     val poll = {
         println("${LocalTime.now()}: START polling")
 
+        val threads = Executors.newCachedThreadPool()
+
         subscriptions
           .parallelStream()
-          .map { fetcher().fetch(it) }
+          .map { fetcher(threads).fetch(it) }
           .filter(Objects::nonNull)
           .forEach { InMemoryFeedRepository.add(it!!) }
+
+        threads.shutdownNow()
 
         println("${LocalTime.now()}: DONE polling")
     }
 
-    private fun fetcher(): Fetcher {
-        val http = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build()
+    private fun fetcher(threads: ExecutorService): Fetcher {
+        val http = HttpClient
+          .newBuilder()
+          .executor(threads)
+          .followRedirects(HttpClient.Redirect.ALWAYS)
+          .build()
         return Fetcher(http)
     }
 }
